0eb6194de1cf0167a10877bf0e79e608601f6973
yein <inwon9892@naver.com>
2023-12-28 17:10:14 +0900

    EX02

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,32 @@
+import java.util.Scanner
+
+// 키보드로 부터 입력을 받아 출력하는 프로그램
+// 1. 키보드로부터 입력는 기능
+// 2. 출력하는 기능
+
+fun main(){
+    // 키보드로 부터 입력을 받는다.
+    val n1 = inputNumber()
+    // 출력한다.
+    printNumber(n1)
+
+}
+
+// 키보드로 부터 입력받는 기능
+fun inputNumber(): Int{
+    val scanner = Scanner(System.`in`)
+    // 키보드로 부터 정수값을 입력받는다.
+    print("숫자를 입력해주세요 : ")
+    val number = scanner.nextInt()
+    // 입력받은 값을 반환한다.
+    return number
+}
+// 출력하는 기능
+fun printNumber(number:Int){
+    if(number % 2 == 0){
+        println("짝수입니다.")
+    } else {
+        println("홀수입니다.")
+    }
+
+}
