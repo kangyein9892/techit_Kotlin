e0f46fe4ee0c6a4084aea5c4e249ba631019225c
yein <inwon9892@naver.com>
2024-01-05 01:27:21 +0900

    Kotlin18_AccessModifier

---
+++ src/main/kotlin/kr/co/lion/pkg1/TestFile2.kt
@@ -0,0 +1,14 @@
+package kr.co.lion.pkg1
+
+open private class PrivateClass3
+open public class PublicClass3
+open internal class InternalClass3
+
+open class SuperClass3{
+
+    private var a30 = 100
+    public var a31 = 200
+    protected var a32 = 300
+    internal var a33 = 400
+
+}
